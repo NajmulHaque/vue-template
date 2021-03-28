5b717dc7f3ef33667e769c1b1a6f604df5172274
Najmul <najmulhoq95@gmail.com>
2021-03-28 17:08:17 +0600

    layout directory add

---
+++ src/layout/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :class="classObj" class="app-wrapper">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <div :class="{'fixed-header':fixedHeader}">
+        <navbar />
+      </div>
+      <app-main />
+    </div>
+  </div>
+</template>
